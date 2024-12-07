5611f732c4b689464de3c3da973a5cbdf4e62d4b
Celal <binaycelalselim@gmail.com>
2024-12-07 23:27:45 +0300

    Update

--- 05_Day_Arrays/examples_and_exercise/exercise.js
+++ 05_Day_Arrays/examples_and_exercise/exercise.js
@@ -104,12 +104,155 @@ if (isIn) {
 
 //! Filter out companies which have more than one 'o' without the filter method
 
+const result = itCompanies.filter((company) => {
+  const oCount = company
+    .toLowerCase()
+    .split("")
+    .filter((char) => char === "o").length;
+  return oCount <= 1;
+});
+
+console.log(result);
+
 //! Sort the array using sort() method
+
+const sortedCompanies = itCompanies.sort();
+console.log(sortedCompanies);
+
 //! Reverse the array using reverse() method
+
+const reversedCompanies = itCompanies.reverse();
+console.log(reversedCompanies);
+
 //! Slice out the first 3 companies from the array
+
+const slicedFirstThree = itCompanies.slice(0, 3);
+console.log(slicedFirstThree);
+
 //! Slice out the last 3 companies from the array
+
+const slicedLastThree = itCompanies.slice(itCompanies.length - 3);
+console.log(slicedLastThree);
+
 //! Slice out the middle IT company or companies from the array
+
+const slicedMiddle = itCompanies.slice(
+  (itCompanies.length - 1) / 2,
+  (itCompanies.length - 1) / 2 + 1
+);
+console.log(slicedMiddle);
+
 //! Remove the first IT company from the array
+
+console.log(itCompanies.shift());
+console.log(itCompanies);
+
 //! Remove the middle IT company or companies from the array
+
+const removedMiddle = itCompanies.splice(itCompanies.length / 2 - 1, 2);
+
+console.log(removedMiddle);
+
 //! Remove the last IT company from the array
+
+const removedEnd = itCompanies.pop();
+console.log(removedEnd);
+
 //! Remove all IT companies
+
+console.log(itCompanies);
+itCompanies.splice(0, itCompanies.length);
+console.log(itCompanies);
+
+//? ******************************************
+console.clear();
+//? ******************************************
+
+//! First remove all the punctuations and change the string to array and count the number of words in the array
+
+let text =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+
+let newText = text.replaceAll(".", "").replaceAll(",", "");
+console.log(newText);
+const newTextArray = newText.split(" ");
+console.log(newTextArray);
+
+//! In the following shopping cart add, remove, edit items
+//* add 'Meat' in the beginning of your shopping cart if it has not been already added
+//* add Sugar at the end of you shopping cart if it has not been already added
+//* remove 'Honey' if you are allergic to honey
+//* modify Tea to 'Green Tea'
+
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+
+shoppingCart.unshift("Meat");
+console.log(shoppingCart);
+
+shoppingCart.push("Sugar");
+console.log(shoppingCart);
+
+console.log(shoppingCart.indexOf("Honey"));
+shoppingCart.splice(4, 1);
+console.log(shoppingCart);
+
+console.log(shoppingCart.indexOf("Tea"));
+shoppingCart.splice(3, 1, "Green Tea");
+console.log(shoppingCart);
+
+//? ******************************************
+console.clear();
+//? ******************************************
+
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+//! In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+if (countries.includes("Ethiopia")) {
+  console.log("ETHIOPIA");
+} else {
+  console.log(countries);
+}
+
+//? ******************************************
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+//! In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+if (webTechs.includes("Sass")) {
+  console.log("Sass is a CSS preprocess");
+} else {
+  webTechs.push("Sass");
+  console.log(webTechs);
+}
+
+//! Concatenate the following two variables and store it in a fullStack variable.
+
+const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
+const backEnd = ["Node", "Express", "MongoDB"];
+
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
+
